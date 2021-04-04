29ae75707c871aedc3978cb207fb27d2c1370e9c
seho Hwang <seho4815@gmail.com>
2021-04-05 00:32:46 +0900

    News 도메인 및 레포지토리 개발

---
+++ src/main/java/com/chicago/server/domain/News.java
@@ -0,0 +1,37 @@
+package com.chicago.server.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class News {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "news_id")
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime writtenTime;
+
+    @Builder
+    public News(String title, String content, String writer){
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+}
